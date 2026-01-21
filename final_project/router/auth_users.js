6f3ca6508bc63c8419e90847764dd08d82be3bf9
TengKimhan <tengkimhan@gmail.com>
2026-01-21 15:25:04 +0700

    Complete Task 7: login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -8,17 +8,52 @@ let users = [];
 const isValid = (username) => {
   //returns boolean
   //write code to check is the username is valid
+  let filtered_users = users.filter((user) => user.username === user);
+  if (filtered_users) {
+    return true;
+  }
+  return false;
 };
 
 const authenticatedUser = (username, password) => {
   //returns boolean
   //write code to check if username and password match the one we have in records.
+  if (isValid(username)) {
+    let filtered_users = users.filter(
+      (user) => user.username === username && user.password === password,
+    );
+    console.log(filtered_users);
+    if (filtered_users) {
+      return true;
+    }
+    return false;
+  }
+  return false;
 };
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
+  let user = req.body.username;
+  let pass = req.body.password;
+  if (!authenticatedUser(user, pass)) {
+    return res.status(403).json({ message: "User not authenticated" });
+  }
+
+  let accessToken = jwt.sign(
+    {
+      data: user,
+    },
+    "access",
+    { expiresIn: 60 * 60 },
+  );
+  req.session.authorization = {
+    accessToken,
+  };
+  return res
+    .status(200)
+    .json({ message: "User logged in successfully", token: accessToken });
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  // return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Add a book review
