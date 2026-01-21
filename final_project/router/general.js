ffefb71966502ae3fc23f21f0459ac3a63d9a8a0
TengKimhan <tengkimhan@gmail.com>
2026-01-21 15:07:39 +0700

    Complete Tast:6 register

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,8 +5,23 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    const present = users.filter((user) => user.username === username);
+    if (present.length === 0) {
+      users.push({ username: req.body.username, password: req.body.password });
+      return res.status(201).json({ message: "User created successfully" });
+    } else {
+      return res.status(400).json({ message: "Already exists" });
+    }
+  } else if (!username && !password) {
+    return res.status(400).json({ message: "Bad request" });
+  } else if (!username || !password) {
+    return res.status(400).json({ message: "Check username and password" });
+  }
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  // return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
